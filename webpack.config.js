1ade0a680463c36beb1fecd71962056f95723f15
mnhss12 <37605170+mnhss12@users.noreply.github.com>
2020-08-23 16:28:49 +0900

    修正:webpack設定修正
    設定途中４

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
 
 module.exports = [
   {
@@ -11,5 +14,33 @@ module.exports = [
       contentBase: path.resolve(__dirname, "dist"), // distディレクトリのファイルを確認する
       port: 3000, // 3000ポートを利用
     }
+  },
+  {
+    entry: {
+      anyname: __dirname + "/src/sass/style.scss", // トランスパイル対象
+    },
+    output: {
+      path: path.join(__dirname, './dist/css'),
+      filename: '[name].css'
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            { loader: MiniCssExtractPlugin.loader },
+            { loader: 'css-loader' },
+            { loader: 'sass-loader' },
+          ],
+        }
+      ],
+    },
+    plugins:[
+      // cssの出力先を指定する
+      new MiniCssExtractPlugin({ filename: 'style/[name].css' }),
+    ],
+    optimization: {
+      minimizer: [new OptimizeCSSAssetsPlugin({})],
+    },
   }
 ];
