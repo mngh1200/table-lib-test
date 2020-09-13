52b17d476b05397a846c7606b42c567eeebeab6a
ghtyf00x <ghtyf00x@gmail.com>
2020-09-13 16:51:48 +0900

    追加:ソートテスト用js追加

---
+++ src/js/listtable.js
@@ -0,0 +1,60 @@
+/**
+ * このライブラリのベースとなる名前空間
+ * @namespace
+ */
+var listtable = listtable || {};
+
+/**
+ * テーブルに関する名前空間
+ * @namespace
+ */
+listtable.table = {};
+
+
+
+/**
+ * データのソート機能の名前空間
+ * @namespace
+ */
+listtable.sort = {};
+
+/**
+ * ソート順昇順を示す定数
+ * @const
+ */
+listtable.sort.ORDER_ASC = 0;
+
+/**
+ * ソート順降順を示す定数
+ * @const
+ */
+listtable.sort.ORDER_DESC = 1;
+
+/**
+ * Object型の配列をソートする<br/>
+ * sortkeysは次のように定義する<br/>
+ * [{name: 'column1', order:listtable.sort.ORDER_ASC}, {name: 'column2', order:listtable.sort.ORDER_DESC}]
+ * @param {Array} data - ソートを行うオブジェクト配列
+ * @param {Array} sortkeys - ソートのキー、並び順のオブジェクト配列
+ * @return {Array} ソートされたdataを返す
+ */
+listtable.sort.sortObject = function(data, sortkeys) {
+  var sortdata = data.concat();
+  sortdata.sort(function(a, b) {
+    var keylen = sortkeys.length;
+    for (var i = 0; i < keylen; i++) {
+      var sortkey = sortkeys[i];
+      if (!(('order' in sortkey) && ('keyname' in sortkey))) continue;
+      if (sortkey.order === listtable.TYPE_ASC) {
+        if (a[sortkey.keyname] < b[sortkey.keyname]) return -1;
+        if (a[sortkey.keyname] > b[sortkey.keyname]) return 1;
+      } else if(sortkey.order === listtable.TYPE_DESC) {
+        if (a[sortkey.keyname] > b[sortkey.keyname]) return -1;
+        if (a[sortkey.keyname] < b[sortkey.keyname]) return 1;
+      }
+    }
+    return 0;
+  });
+  return sortdata;
+}
+
