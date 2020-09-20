782b19966ed9ddf15a0a7a4162b7e345f3254d1c
ghtyf00x <ghtyf00x@gmail.com>
2020-09-20 11:31:30 +0900

    修正：listtable.js
    ソート定数の修正

--- src/js/listtable.js
+++ src/js/listtable.js
@@ -33,7 +33,7 @@ listtable.sort.ORDER_DESC = 1;
 /**
  * Object型の配列をソートする<br/>
  * sortkeysは次のように定義する<br/>
- * [{name: 'column1', order:listtable.sort.ORDER_ASC}, {name: 'column2', order:listtable.sort.ORDER_DESC}]
+ * [{keyname: 'column1', order:listtable.sort.ORDER_ASC}, {keyname: 'column2', order:listtable.sort.ORDER_DESC}]
  * @param {Array} data - ソートを行うオブジェクト配列
  * @param {Array} sortkeys - ソートのキー、並び順のオブジェクト配列
  * @return {Array} ソートされたdataを返す
@@ -45,10 +45,10 @@ listtable.sort.sortObject = function(data, sortkeys) {
     for (var i = 0; i < keylen; i++) {
       var sortkey = sortkeys[i];
       if (!(('order' in sortkey) && ('keyname' in sortkey))) continue;
-      if (sortkey.order === listtable.TYPE_ASC) {
+      if (sortkey.order === listtable.ORDER_ASC) {
         if (a[sortkey.keyname] < b[sortkey.keyname]) return -1;
         if (a[sortkey.keyname] > b[sortkey.keyname]) return 1;
-      } else if(sortkey.order === listtable.TYPE_DESC) {
+      } else if(sortkey.order === listtable.ORDER_DESC) {
         if (a[sortkey.keyname] > b[sortkey.keyname]) return -1;
         if (a[sortkey.keyname] < b[sortkey.keyname]) return 1;
       }
